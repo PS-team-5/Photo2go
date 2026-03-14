f1a47d1140b2d602ced67e0fc3cd7dffff628402
ErnestasCodes <pleikys.az@gmail.com>
2026-03-14 20:38:24 +0200

    #SCRUM-7/subtask#32,34 Add login backend

--- backend/Photo2GoAPI/Program.cs
+++ backend/Photo2GoAPI/Program.cs
@@ -1,14 +1,17 @@
 using Microsoft.EntityFrameworkCore;
 using Photo2GoAPI.Data;
+using Photo2GoAPI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<UserService>();
 
 var app = builder.Build();
 
@@ -21,11 +24,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-
+app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
