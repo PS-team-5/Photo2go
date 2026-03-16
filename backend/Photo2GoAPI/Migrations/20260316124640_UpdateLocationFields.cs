f928b14ec30388cc188380b7f56e6596e47f0b54
Redas Baltrenas <reduxas02@gmail.com>
2026-03-16 15:29:26 +0200

    #SCRUM-12 Sukurtas Location modelis ir pridėti Vilniaus lankytinų vietų duomenys

---
+++ backend/Photo2GoAPI/Migrations/20260316124640_UpdateLocationFields.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Photo2GoAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateLocationFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "BuildingMaterials",
+                table: "Locations",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Category",
+                table: "Locations",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Province",
+                table: "Locations",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UnescoStatus",
+                table: "Locations",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BuildingMaterials",
+                table: "Locations");
+
+            migrationBuilder.DropColumn(
+                name: "Category",
+                table: "Locations");
+
+            migrationBuilder.DropColumn(
+                name: "Province",
+                table: "Locations");
+
+            migrationBuilder.DropColumn(
+                name: "UnescoStatus",
+                table: "Locations");
+        }
+    }
+}
---
+++ backend/Photo2GoAPI/Model/Location.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Photo2GoAPI.Model;
+[Table("Locations")]
+
+public class Location
+{
+    [Key]
+    [Column("id")]
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+    public string ObjectType { get; set; } = null!;
+    public string ArchitectureStyle { get; set; } = null!;
+    public string Period { get; set; } = null!;
+    public string City { get; set; } = null!;
+    public string Province { get; set; } = null!;
+    public string Category { get; set; } = null!;
+    public string BuildingMaterials { get; set; } = null!;
+    public string IsUnescoProtected { get; set; }
+}
