9d666655d2e0e330c33a3c3cf7cb1dc1eea08551
Karolis Rimkus <k.rimkus@stud.vilniustech.lt>
2026-04-04 18:32:19 +0300

    #SCRUM-70 Backend filtravimo logika pagal kategoriją

---
+++ backend/Photo2GoAPI/Controllers/LocationsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Photo2GoAPI.Data; 
+using System.Linq;
+
+namespace Photo2GoAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LocationsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public LocationsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("filter")]
+        public IActionResult FilterLocations([FromQuery] string? objectType)
+        {
+            var locations = _context.Locations.AsQueryable();
+
+            if (!string.IsNullOrEmpty(objectType))
+            {
+                locations = locations.Where(l => l.ObjectType == objectType);
+            }
+
+            return Ok(locations.ToList());
+        }
+    }
+}
